4d73385f44f48af84e1096bfbf2bbbbc163bfaca
Don Ward <10is0420@gmail.com>
2019-08-19 14:02:23 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -5,14 +5,10 @@ class Project
   def initialize(title)
     @title = title
     @backers = []
-
   end
   
   def add_backer(backer)
     @backers << backer
-    
-
+    backer.backed_projects << self
   end
-  
-  
 end

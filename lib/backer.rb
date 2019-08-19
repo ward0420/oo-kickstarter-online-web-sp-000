1a24132e0a51d760feb62f85551ba770a60a0c63
Don Ward <10is0420@gmail.com>
2019-08-19 13:28:23 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -9,18 +9,7 @@ class Backer
   
   def back_project(name)
     @backed_projects << name
-    project.add_backer(self)
+    # project.add_backer(self)
   end
   
-#   describe 'Backer - More Advanced #back_project' do
-#   it 'also adds the backer to the project\'s backers array' do
-#     logan = Backer.new("Logan")
-#     hoverboard = Project.new("Awesome Hoverboard")
-#     logan.back_project(hoverboard)
-
-#     # If by this point, the project knows about a new backer, where does that mean
-#     # some more magic needs to happen? There's really only one place it can happen.
-#     expect(hoverboard.backers).to include(logan)
-#   end
-# end
 end
